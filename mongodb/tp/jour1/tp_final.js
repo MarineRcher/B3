7213597434c523064a0e308f10a083ac272c883a
marine <marine.richer41@gmail.com>
2025-03-04 12:49:36 +0100

    finish tp final jour1

--- mongodb/tp/jour1/tp_final.js
+++ mongodb/tp/jour1/tp_final.js
@@ -361,10 +361,23 @@ db.emprunt.insertMany([
 ]);
 
 // Comparez cette approche avec celle où les emprunts sont directement intégrés dans le document utilisateur
-// Cette approche est flexible, si on change le titre du livre par exemple, on aura pas a le changer dans livres_empruntes des utilisateurs.
+// Cette approche est flexible, si on change le titre du livre par exemple, on n'aura pas à le changer dans livres_empruntes des utilisateurs.
 
 // 1. Quels sont les avantages et inconvénients de chaque approche ?
+// Approche avec emprunts dans les utilisateurs :
+// avantages : L'accès aux livres empruntés plus rapide avec une requête plus simple.
+// Inconvénients : Peu flexible (Si le titre est modifié, il faut le changer dans 2 champs.)
+// Pour des requêtes sur l'ensemble des emprunts, cela devient vite une contrainte
+// Approche avec les références :
+// avantages : pas de duplication de données, meilleure modélisation, plus simple de faire des
+// requetes sur l'ensemble des emprunts
+// Inconvénients : requêtes plus complexes
 
 // 2. Quelle approche privilégieriez-vous pour une application réelle et pourquoi ?
+// On va privilégier l'approche avec les références, car c'est mieux maintenable, plus facile.
+// d'accès a l'ensemble des données, que ce soit pour envoyer un mail aux emprunts en retard
+// ou récupère le nombre de livres empruntés dans le mois.
 
 // 3. Comment modéliseriez-vous les cas où un même livre peut exister en plusieurs exemplaires ?
+// On va faire comme emprunts, en créant une nouvelle collection avec pour référence le livre
+// on ajoute des champs comme disponible, son état, son emplacement. Ensuite, dans la collection emprunt, on change l'objet du livre par celui de l'exemplaire.
